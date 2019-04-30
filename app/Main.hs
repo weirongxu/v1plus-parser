c4a56d05b29c3d4b8d286a17ac522f7ce2caf3be
Weirong Xu <weirongxu.raidou@gmail.com>
2019-04-30 18:35:50 +0800

    update description

--- app/Main.hs
+++ app/Main.hs
@@ -11,15 +11,15 @@ data CliOptions = CliOptions
 optionBuilder :: Parser CliOptions
 optionBuilder = CliOptions
   <$> strArgument
-      ( metavar "SOURCE" <> help "解析dat文件" )
+      ( metavar "SOURCE" <> help "需要解析的dat文件" )
   <*> flag False True
       ( long "pretty"
       <> short 'p'
-      <> help "格式化" )
+      <> help "是否格式化" )
   <**> helper
 
 cliBuilder :: ParserInfo CliOptions
-cliBuilder = info optionBuilder $ fullDesc <> progDesc "解析好记星V1+学习机"
+cliBuilder = info optionBuilder $ fullDesc <> progDesc "好记星V1+学习机文件格式解析"
 
 main :: IO ()
 main = mainParse =<< execParser cliBuilder
